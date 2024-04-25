a2638036372684199536e2c3f4a3fc28826ff491
arnabw <arnabsahawrk@gmail.com>
2024-04-25 14:19:29 +0600

    server setup

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+
+//config
+require("dotenv").config();
+const port = process.env.PORT || 8080;
+const app = express();
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+//Database Authenticate
+const uri = "mongodb://localhost:27017";
+
+app.get("/", (req, res) => {
+  res.send(`Server is running on ${port}`);
+});
+
+app.listen(port, () => {
+  console.log(`Server connected on ${port}`);
+});
