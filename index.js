60b2d26d88cb40eb2fc25a8fb9e6c4654424e250
arnabw <arnabsahawrk@gmail.com>
2024-05-01 19:45:29 +0600

    MongoDB intigrate

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 //config
 require("dotenv").config();
@@ -11,7 +12,25 @@ app.use(cors());
 app.use(express.json());
 
 //Database Authenticate
-const uri = "mongodb://localhost:27017";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2p5zaxk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+//Send and Get data from sever to database
+async function run() {
+  try {
+  } catch (err) {
+    console.log("Error from database:", err);
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send(`Server is running on ${port}`);
